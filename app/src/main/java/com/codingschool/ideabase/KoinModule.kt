48255643dc2431f7f2f4c383b91f600b0bbf2d94
Wim Janssen <wim.janssen@edu.fh-kaernten.ac.at>
2021-07-20 14:54:24 +0200

    add okhhtp with dummy base auth

--- app/src/main/java/com/codingschool/ideabase/KoinModule.kt
+++ app/src/main/java/com/codingschool/ideabase/KoinModule.kt
@@ -1,38 +1,55 @@
 package com.codingschool.ideabase
 
 import androidx.room.Room
+import com.codingschool.ideabase.utils.baseUrl
+import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 val appModule = module {
 
-  /*  single<AppDatabase> { (
-        Room
-            .databaseBuilder(
-                androidContext(),
-                AppDatabase::class.java,
-                "app-db"
-            )
-            // REMOVE ON PRODUCTION VERSION
-            .fallbackToDestructiveMigration()
-            .allowMainThreadQueries()
+    /*  single<AppDatabase> { (
+          Room
+              .databaseBuilder(
+                  androidContext(),
+                  AppDatabase::class.java,
+                  "app-db"
+              )
+              // REMOVE ON PRODUCTION VERSION
+              .fallbackToDestructiveMigration()
+              .allowMainThreadQueries()
+              .build()
+              )
+      }*/
+    single {
+        OkHttpClient.Builder()
+            .addNetworkInterceptor { chain ->
+                chain.proceed(
+                    chain.request().newBuilder()
+                        .header(
+                            "Authorization",
+                            "Basic d2ltQG1haWwuY29tOndpbTEyMzQ1Njc4"
+                        )
+                        .build()
+                )
+            }
             .build()
-            )
-    }*/
+    }
 
-   /* single {
+    single {
         Retrofit.Builder()
-            .baseUrl( baseUrl)
+            .client(OkHttpClient())
+            .baseUrl(baseUrl)
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-
-
-    }*/
+    }
 /*
     factory { provideUserApi(get()) }*/
-
 
 
 }
---
+++ app/src/main/java/com/codingschool/ideabase/utils/Const.kt
@@ -0,0 +1,5 @@
+package com.codingschool.ideabase.utils
+
+const val baseUrl = "https://ideenmanagement.tailored-apps.com/api"
+const val testusername = "wim@mail.com"
+const val testpwd = "wim12345678"
